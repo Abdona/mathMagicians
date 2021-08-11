3c92e8bd35ca7b8bb7341a805bd85378b17f2528
Abdulrahman <boodynaser@rocketmail.com>
2021-08-11 22:24:13 +0200

    use app component as wrapper

---
+++ src/App.js
@@ -0,0 +1,11 @@
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
+import MyCalculator from './components/Calculator';
+
+class App extends React.Component {
+  render() {
+    return <MyCalculator />;
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './Calculator.css';
-import MyCalculator from './components/Calculator';
+import './components/Calculator.css';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <MyCalculator />
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
