ce2da04e272760687bcaedda540aa11e56240ca2
Abdulrahman <boodynaser@rocketmail.com>
2021-08-11 17:56:38 +0200

    use grid to style the calculator

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,12 +1,32 @@
+/* eslint-disable react/button-has-type */
+/* eslint-disable no-useless-constructor */
+/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 
-export default class MyButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = props;
-  }
-
+export default class MyCalculator extends React.Component {
   render() {
-    return <button type="button">{this.state}</button>;
+    return (
+      <div className="grid-continer">
+        <button>AC</button>
+        <button>+/-</button>
+        <button>%</button>
+        <button className="buttoperation">/</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button className="buttoperation">x</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button className="buttoperation">-</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button className="buttoperation">+</button>
+        <button>0</button>
+        <button>.</button>
+        <button className="buttoperation">=</button>
+      </div>
+    );
   }
 }
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MyCalculator from './components/Buttons';
+import './index.css';
+
+ReactDOM.render(
+  <MyCalculator />,
+  document.getElementById('root'),
+);
