804635c7e016704de69932fd38b8bb87ce0685ea
Abdulrahman <boodynaser@rocketmail.com>
2021-08-13 16:28:07 +0200

    create calchandler and bind with buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,36 +1,52 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/no-access-state-in-setstate */
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-unused-expressions */
 /* eslint-disable react/no-unused-state */
 /* eslint-disable react/button-has-type */
 import React from 'react';
+import calculate from '../logic/calculate';
 
 export default class MyCalculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { total: 0 };
+    this.screenString = 0;
+    this.state = { total: this.screenString, next: '', operation: '' };
+    this.calchandler = this.calchandler.bind(this);
+  }
+
+  // buttonhandler(e) {
+  //   this.screenString += e.target.innerText;
+  //   this.setState({ total: this.screenString });
+  // }
+
+  calchandler(e) {
+    this.setState(calculate(this.state, e.target.innerText));
   }
 
   render() {
     return (
       <div className="grid-continer">
-        <input className="resultscreen" placeholder="0" />
-        <button>AC</button>
-        <button>+/-</button>
-        <button>%</button>
-        <button className="buttoperation">/</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button className="buttoperation">x</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button className="buttoperation">-</button>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button className="buttoperation">+</button>
-        <button className="zeroitem">0</button>
-        <button>.</button>
-        <button className="buttoperation">=</button>
+        <input className="resultscreen" readOnly placeholder={this.state.total} />
+        <button onClick={this.calchandler}>AC</button>
+        <button onClick={this.calchandler}>+/-</button>
+        <button onClick={this.calchandler}>%</button>
+        <button onClick={this.calchandler} className="buttoperation">/</button>
+        <button onClick={this.calchandler}>7</button>
+        <button onClick={this.calchandler}>8</button>
+        <button onClick={this.calchandler}>9</button>
+        <button onClick={this.calchandler} className="buttoperation">x</button>
+        <button onClick={this.calchandler}>4</button>
+        <button onClick={this.calchandler}>5</button>
+        <button onClick={this.calchandler}>6</button>
+        <button onClick={this.calchandler} className="buttoperation">-</button>
+        <button onClick={this.calchandler}>1</button>
+        <button onClick={this.calchandler}>2</button>
+        <button onClick={this.calchandler}>3</button>
+        <button onClick={this.calchandler} className="buttoperation">+</button>
+        <button onClick={this.calchandler} className="zeroitem">0</button>
+        <button onClick={this.calchandler}>.</button>
+        <button onClick={this.calchandler} className="buttoperation">=</button>
       </div>
     );
   }
