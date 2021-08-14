b78c81e0e9974fb1d2635c22a8adb178ff3c5e3a
Abdulrahman <boodynaser@rocketmail.com>
2021-08-14 14:01:51 +0200

    add operation and result on calculator screen

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,3 +1,4 @@
+/* eslint-disable class-methods-use-this */
 /* eslint-disable react/no-access-state-in-setstate */
 /* eslint-disable react/no-unused-state */
 /* eslint-disable react/destructuring-assignment */
@@ -9,23 +10,56 @@ import calculate from '../logic/calculate';
 export default class MyCalculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { total: 0, next: 0, operation: '' };
+    this.num = '';
+    this.value = 0;
+    this.state = {
+      screen: 0, total: 0, next: 0, operation: '',
+    };
     this.calchandler = this.calchandler.bind(this);
   }
 
   calchandler(e) {
+    // if (e.target.innerText === 'AC') {
+    //   // this.value = 0;
+    //   // this.num = '';
+    //   const { total, next, operation } = calculate(this.state, e.target.innerText);
+    //   // alert(total);
+    //   this.setState({
+    //     screen: 0, total, next, operation,
+    //   });
+    // } else {
+    //   // this.num += e.target.innerText;
+    //   const { total, next, operation } = calculate(this.state, e.target.innerText);
+    //   alert(total);
+    //   // this.value = total;
+    //   // alert(this.value);
+    //   this.setState({
+    //     screen: this.num, total, next, operation,
+    //   });
+    // }
     if (e.target.innerText === 'AC') {
-      const { next, operation } = calculate(this.state, e.target.innerText);
-      this.setState({ total: 0, next, operation });
+      this.num = '';
+      const { total, next, operation } = calculate(this.state, e.target.innerText);
+      this.setState({
+        screen: 0, total, next, operation,
+      });
+    } else if (e.target.id === 'eqoperation') {
+      const { total, next, operation } = calculate(this.state, e.target.innerText);
+      this.setState({
+        screen: total, total, next, operation,
+      });
+      // alert(this.state.total);
     } else {
-      this.setState(calculate(this.state, e.target.innerText));
+      this.num += e.target.innerText;
+      this.setState({ screen: this.num, ...calculate(this.state, e.target.innerText) });
+      // alert(this.state.total);
     }
   }
 
   render() {
     return (
       <div className="grid-continer">
-        <input className="resultscreen" readOnly placeholder={this.state.total} />
+        <input className="resultscreen" readOnly placeholder={this.state.screen} />
         <button onClick={this.calchandler}>AC</button>
         <button onClick={this.calchandler}>+/-</button>
         <button onClick={this.calchandler}>%</button>
@@ -44,7 +78,7 @@ export default class MyCalculator extends React.Component {
         <button onClick={this.calchandler} className="buttoperation">+</button>
         <button onClick={this.calchandler} className="zeroitem">0</button>
         <button onClick={this.calchandler}>.</button>
-        <button onClick={this.calchandler} className="buttoperation">=</button>
+        <button onClick={this.calchandler} id="eqoperation" className="buttoperation">=</button>
       </div>
     );
   }
